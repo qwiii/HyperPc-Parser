a5efec45d67e9ef9fbc5bc25e9e582a33749740b
qwiii <120501525+qwiii@users.noreply.github.com>
2023-10-07 14:30:50 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from hyperpc import *
+from pprint import pprint
+
+
+def main(hyper: HyperPc) -> None:
+    print(f"{COLOR}{LOGO}Created by Qwiii\n")
+
+    for x, value in enumerate(CATEGORIES, 1):
+        print(f"{COLOR}{x}::{value}")
+
+    user_input = int(input(f"{COLOR}Select category:: "))
+
+    match user_input:
+        case 1:
+            hyper.parse_computers_catalog()
+            print("The result is saved in Catalog.json")
+        case 2:
+            hyper.parse_optimal_pc()
+            print("The result is saved in Optimal.json")
+        case 3:
+            hyper.parse_powerful_pc()
+            print("The result is saved in Powerful.json")
+        case 4:
+            hyper.parse_custom_pc()
+            print("The result is saved in Custom.json")
+        case 5:
+            pprint(hyper.parse_hardware(url=input(f"{COLOR}Product Link::: ")))
+        case _:
+            print("You entered an incorrect number!")
+
+
+if __name__ == "__main__":
+    main(HyperPc())
