0e4d7b0cfeeb621ff4f00a5ba4a4bf3324ec4cca
qwiii <120501525+qwiii@users.noreply.github.com>
2023-10-07 19:58:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ def main(hyper: HyperPc) -> None:
     match user_input:
         case 1:
             hyper.parse_computers_catalog()
-            print("The result is saved in Catalog.json")
+            print(f"{COLOR}The result is saved in Catalog.json")
         case 2:
             hyper.parse_optimal_pc()
-            print("The result is saved in Optimal.json")
+            print(f"{COLOR}The result is saved in Optimal.json")
         case 3:
             hyper.parse_powerful_pc()
-            print("The result is saved in Powerful.json")
+            print(f"{COLOR}The result is saved in Powerful.json")
         case 4:
             hyper.parse_custom_pc()
-            print("The result is saved in Custom.json")
+            print(f"{COLOR}The result is saved in Custom.json")
         case 5:
             pprint(hyper.parse_hardware(url=input(f"{COLOR}Product Link::: ")))
         case _:
-            print("You entered an incorrect number!")
+            print(f"{COLOR}You entered an incorrect number!")
 
 
 if __name__ == "__main__":
